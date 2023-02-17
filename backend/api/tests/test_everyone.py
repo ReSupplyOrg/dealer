7c35cfa19cb390381146eb42b8d169a3d2913720
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2023-02-17 13:34:57 -0500

    feat: update tests searchStores

--- backend/api/tests/test_everyone.py
+++ backend/api/tests/test_everyone.py
@@ -43,4 +43,34 @@ class TestClients(TestCase):
         response = self.client.post(self.seach_stores_url,format='json',data=raw_data3,HTTP_session=token["token"])
         self.assertEquals(response.status_code,200)
 
-        
+    def test_search_stores_no_arguments(self):
+        raw_data = {
+            "phone": "1234567",
+            "name": "Domihoes",
+            "username": "domihoes",
+            "password": "12345",
+            "address": "Calle 13"
+        }
+        response = self.client.put(self.stores_register_url,format='json',data=raw_data)
+
+        self.assertEquals(response.status_code,200)
+
+        raw_data2 = {
+            "username": "domihoes",
+            "password": "12345",
+        }
+        response = self.client.post(self.stores_login_url,format='json',data=raw_data2)
+        self.assertEquals(response.status_code,200)
+
+        token = json.loads(response.content.decode('utf-8'))
+        raw_data3 = {
+        "name": "tiendanoexistente",
+        "phone": "",
+        "rating": 0,
+        "address": "",
+        "username": "",
+        "page": 0
+        }
+        self.client.credentials(session=token["token"])
+        response = self.client.post(self.seach_stores_url,format='json',data=raw_data3,HTTP_session=token["token"])
+        self.assertEquals(response.data,{"totalPages": 1,"items": []})
