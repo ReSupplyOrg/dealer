2407c9e823b4168002e96423d31a0ce18337f06c
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2023-02-16 20:46:31 -0500

    feat: tests folder created & test_urls done

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.echo),
     #stores urls
-    path('stores/register/', views.storesRegister),
-    path('stores/login/', views.storesLogin),
-
+    path('stores/register/', views.storesRegister, name="stores_register"),
+    path('stores/login/', views.storesLogin, name="stores_login"),
+    path('stores/account/',views.storesAccount, name="stores_account"),
     #clients urls
-    path('clients/register/', views.clientsRegister),
-    path('clients/login/',views.clientsLogin),
-    path('clients/account/',views.clientsAccount)
+    path('clients/register/', views.clientsRegister, name="clients_register"),
+    path('clients/login/',views.clientsLogin, name="clients_login"),
+    path('clients/account/',views.clientsAccount, name="clients_account")
 ]
