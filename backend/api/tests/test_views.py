498818c435a18a4f8999aedf953b26d4531de17b
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2023-02-16 23:08:40 -0500

    feat: test_stores update

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -1,7 +1,6 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from rest_framework.test import APIClient
 from django.urls import reverse
-from api.models import Stores, Clients
 import json
 
 class TestStores(TestCase):
@@ -11,12 +10,13 @@ class TestStores(TestCase):
         self.echo_url = reverse('echo')
         self.stores_register_url = reverse("stores_register")
         self.stores_login_url = reverse("stores_login")
+        self.stores_account_url = reverse("stores_account")
 
     def test_echo_GET(self):
         response = self.client.get(self.echo_url)
         self.assertEquals(response.status_code, 200)
 
-    def test_stores_register_login_success(self):
+    def test_stores_register_login_account_success(self):
         raw_data = {
             "phone": "1234567",
             "name": "Domihoes",
@@ -34,8 +34,19 @@ class TestStores(TestCase):
         }
         response = self.client.post(self.stores_login_url,format='json',data=raw_data2)
         self.assertEquals(response.status_code,200)
-        
-    def test_stores_register_login_bad_credentials(self):
+
+        token = json.loads(response.content.decode('utf-8'))
+        raw_data3 = {
+            "phone": "1234567",
+            "name": "Dominoes",
+            "password": "1234567890",
+            "address": "Calle 13"
+        }
+        self.client.credentials(session=token["token"])
+        response = self.client.patch(self.stores_account_url,format='json',data=raw_data3,HTTP_session=token["token"])
+        self.assertEquals(response.status_code,200)
+
+    def test_stores_register_login_bad_username(self):
             raw_data = {
                 "phone": "1234567",
                 "name": "Domihoes",
@@ -53,3 +64,52 @@ class TestStores(TestCase):
             }
             response = self.client.post(self.stores_login_url,format='json',data=raw_data2)
             self.assertEquals(response.status_code,401)
+
+    def test_stores_register_login_bad_password(self):
+            raw_data = {
+                "phone": "1234567",
+                "name": "Domihoes",
+                "username": "domihoes",
+                "password": "12345",
+                "address": "Calle 13"
+            }
+            response = self.client.put(self.stores_register_url,format='json',data=raw_data)
+
+            self.assertEquals(response.status_code,200)
+
+            raw_data2 = {
+                "username": "domihoes",
+                "password": "123456",
+            }
+            response = self.client.post(self.stores_login_url,format='json',data=raw_data2)
+            self.assertEquals(response.status_code,401)
+
+    def test_stores_register_login_account_bad_token(self):
+        raw_data = {
+            "phone": "1234567",
+            "name": "Domihoes",
+            "username": "domihoes",
+            "password": "12345",
+            "address": "Calle 13"
+        }
+        response = self.client.put(self.stores_register_url,format='json',data=raw_data)
+
+        self.assertEquals(response.status_code,200)
+
+        raw_data2 = {
+            "username": "domihoes",
+            "password": "12345",
+        }
+        response = self.client.post(self.stores_login_url,format='json',data=raw_data2)
+        self.assertEquals(response.status_code,200)
+
+        token = "bad_token"
+        raw_data3 = {
+            "phone": "1234567",
+            "name": "Dominoes",
+            "password": "1234567890",
+            "address": "Calle 13"
+        }
+        self.client.credentials(session=token)
+        response = self.client.patch(self.stores_account_url,format='json',data=raw_data3,HTTP_session=token)
+        self.assertEquals(response.status_code,401)
